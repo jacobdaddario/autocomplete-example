494ca9568660893de1c2fd7c0783888c933a9583
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-04 01:36:06 +0800

    Delete config directory

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  root to: "books#index"
-
-  resources :books, only: [:index]
-  get '/search', to: 'search#search'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
